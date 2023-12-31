83ce96b1212e37cc983ea5dde2f786158961310a
Aellun <okeelo.k.otieno@gmail.com>
2023-12-31 21:51:50 +0300

    responsive landing page

---
+++ Empire-internet-provider/README.md
@@ -0,0 +1,5 @@
+## Project name: Empire Internet Provider landing page
+
+this is an DecoderBot internship project
+
+I was required to create A responsive product/software landing page
---
+++ Empire-internet-provider/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Message submitted successfully!'); // You can replace this with actual form submission logic.
+});
